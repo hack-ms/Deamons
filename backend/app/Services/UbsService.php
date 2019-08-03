af6758ff0aecf4c1026b3e70e588e0692b87dfb4
Dturati <davidturati@gmail.com>
2019-08-03 19:58:29 -0400

    end-point obs filtro

--- backend/app/Services/UbsService.php
+++ backend/app/Services/UbsService.php
@@ -28,4 +28,44 @@ class UbsService
     {
             return Ubs::all();
     }
+
+    /**
+     * @param $request
+     * @return mixed
+     * @api
+     * @host http://localhost:8000/api/ubs/filtro
+     * @get
+     * {
+                "co_cep":"768880011110",
+                "no_fantasia":"POSTO DE SAUDE DE FIGUEIRA DO OESTE",
+                "no_bairro":"POVOADO",
+                "co_banana":"maça"
+            }
+     *
+     * @return {
+    *   {
+                "gid": 1,
+                "co_cnes": 2735180,
+                "lat": "-23.7792527675622",
+                "long": "-52.3512911796555",
+                "no_fantasia": "POSTO DE SAUDE DE FIGUEIRA DO OESTE",
+                "no_logradouro": "AVENIDA PIQUIRI",
+                "nu_endereco": "S\/N",
+                "no_bairro": "DISTRITO",
+                "nu_telefone": "(44)3533-1180",
+                "co_cep": "87270000",
+                "uf": "PR",
+                "cidade": "Engenheiro Beltrão"
+                }
+     * }
+     *
+     */
+    public function getValuesFilters($request)
+    {
+        $requestFiltered = FiltersValidateRequest::validadeObs($request);
+        return Ubs::where('no_fantasia','like',$requestFiltered['no_fantasia'])
+            ->orWhere('co_cep',$requestFiltered['co_cep'])
+            ->orWhere('no_bairro',$requestFiltered['no_bairro'])
+            ->first();
+    }
 }
