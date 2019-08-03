a1db295693b3d5bb47a084d218bbafe0a10051f4
Vagner Garcia <garciavagnata.vg@gmail.com>
2019-08-03 18:13:37 -0400

    Implementa post inicial para avaliacao

--- backend/app/Services/AvaliacaoService.php
+++ backend/app/Services/AvaliacaoService.php
@@ -4,25 +4,18 @@ namespace App\Services;
 
 use App\Avaliacao;
 use App\Exceptions\FalhaInserirException;
-use Illuminate\Support\Fluent;
 
 class AvaliacaoService
 {
     public function save(array $data): Avaliacao
     {
-        $data      = new Fluent($data);
         $avaliacao = new Avaliacao();
-
-//        $atributtes[''] = [
-//
-//        ]
-
         $avaliacao->fill($data);
 
         try {
             $avaliacao->save();
         } catch (\Exception $exception) {
-            throw new FalhaInserirException('Falha ao inserir avaliacao: ', $exception->getMessage());
+            throw new FalhaInserirException('Falha ao inserir avaliacao: ' . $exception->getMessage());
         }
 
         return $avaliacao;
