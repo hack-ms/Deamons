5d69469c6406ac1103a3d92fad5f051b5f07adb0
Vagner Garcia <garciavagnata.vg@gmail.com>
2019-08-03 14:13:59 -0400

    Modelo Ubs

--- backend/app/Ubs.php
+++ backend/app/Ubs.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ubs extends Model
 {
-    //
+    protected $fillable = [
+        'gid',
+        'co_cnes',
+        'lat',
+        'long',
+        'no_fantasia',
+        'no_logradouro',
+        'nu_endereco',
+        'no_bairro',
+        'nu_telefone',
+        'co_cep',
+        'uf',
+        'cidade'
+    ];
+
+    public $timestamps = false;
 }
