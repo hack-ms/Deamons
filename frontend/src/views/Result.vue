511ba21fa892451fb87a105b6a5772d04a3c2f3f
Igor Trindade <igorhideki@gmail.com>
2019-08-04 00:29:20 -0400

    Add componente Result

--- frontend/src/views/Result.vue
+++ frontend/src/views/Result.vue
@@ -5,10 +5,26 @@
     </h1>
     <div
       v-for="item in list"
-      :key="item.cnes"
-      class="result__list"
+      :key="item.co_cnes"
+      class="result__item"
     >
-      <h3>{{ item.nome }}</h3>
+      <h3 class="result__name">
+        {{ item.no_fantasia }}
+      </h3>
+      <div class="result__info">
+        <span class="result__icon fa fa-map-marker" />
+        <span>
+          {{ item.no_logradouro }},
+          {{ item.nu_endereco }},
+          {{ item.no_bairro }}
+        </span>
+      </div>
+      <div class="result__info">
+        <span class="result__icon fa fa-map-marker" />
+        <span>
+          {{ item.cidade }} - {{ item.uf }}
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -20,9 +36,31 @@ export default {
     return {
       list: [
         {
-          cnes: '2134',
-          nome: 'Aero Rancho',
-          endereco: 'Av. Rachel de Queiroz, 995',
+          gid: 1,
+          co_cnes: 2735180,
+          lat: '-23.7792527675622',
+          long: '-52.3512911796555',
+          no_fantasia: 'POSTO DE SAUDE DE FIGUEIRA DO OESTE',
+          no_logradouro: 'AVENIDA PIQUIRI',
+          nu_endereco: 'S/N',
+          no_bairro: 'DISTRITO',
+          nu_telefone: '(44)3533-1180',
+          co_cep: '87270000',
+          uf: 'PR',
+          cidade: 'Engenheiro Beltrão',
+        },
+        {
+          gid: 1,
+          co_cnes: 424233,
+          lat: '-23.7792527675622',
+          long: '-52.3512911796555',
+          no_fantasia: 'AERO RANCHO',
+          no_logradouro: 'AVENIDA PIQUIRI',
+          nu_endereco: '280',
+          no_bairro: 'DISTRITO',
+          nu_telefone: '(44)3533-1180',
+          co_cep: '87270000',
+          uf: 'MS',
           cidade: 'Campo Grande',
         },
       ],
@@ -33,7 +71,41 @@ export default {
 
 <style lang="scss" scoped>
 .result {
+  height: 100vh;
   padding: 2.4rem;
   background-color: #fff;
+
+  &__item {
+    padding-bottom: 2.6rem;
+    border-bottom: 0.2rem solid $border-color;
+
+    & + & {
+      margin-top: 3.2rem;
+    }
+  }
+
+  &__title {
+    color: $primary;
+    font-family: $font-head;
+    font-size: 2.4rem;
+    text-align: center;
+    margin: 2.4rem 0 4rem;
+  }
+
+  &__name {
+    font-size: 2.2rem;
+    font-weight: 900;
+    color: $text-dark;
+    margin-bottom: 2.4rem;
+  }
+
+  &__info {
+    margin-bottom: 0.8rem;
+  }
+
+  &__icon {
+    margin-right: 0.8rem;
+  }
+
 }
 </style>
