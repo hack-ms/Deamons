dfa129493263470eceff8781d5bedc5ea8e79da5
Igor Trindade <igorhideki@gmail.com>
2019-08-03 22:28:51 -0400

    Add componente InputAction

---
+++ frontend/src/components/InputAction.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="input-action">
+    <span :class="classIcon" />
+    <input
+      class="input-action__entry"
+      type="text"
+      :placeholder="placeholder"
+    >
+    <span
+      :class="classIconButton"
+      @click="handler"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputAction',
+  props: {
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    classIcon: {
+      type: String,
+      default: '',
+    },
+    classIconButton: {
+      type: String,
+      default: '',
+    },
+    handler: {
+      type: Function,
+      default: () => () => {},
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.input-action {
+  background-color: #fff;
+  width: 100%;
+  height:7.2rem;
+  display: flex;
+  border-radius: 0.8rem;
+  box-shadow: $box-shadow-level-1;
+  padding: 1.6rem;
+  font-size: 1.8rem;
+  align-items: center;
+
+  &__entry {
+    border: 0;
+    width: 100%;
+    padding: 0 1.6rem;
+    font-size: 1.8rem;
+
+    &::placeholder {
+      color: $text-light;
+    }
+  }
+}
+</style>
